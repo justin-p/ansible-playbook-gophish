7c4c91a24e72582b24db09c14aca163977158dfd
Justin Perdok <justin-p@users.noreply.github.com>
2020-11-19 19:58:43 +0000

    Update main.tf

--- ansible_and_terraform/digitalocean_cloudflare/terraform_playbooks/DNS/main.tf
+++ ansible_and_terraform/digitalocean_cloudflare/terraform_playbooks/DNS/main.tf
@@ -4,7 +4,7 @@ resource "cloudflare_record" "a_host_fqdn" {
   value   = var.cf_host_ipv4
   type    = "A"
   ttl     = var.cf_record_ttl
-  proxied = var.cf_record_proxied
+  proxied = false
 }
 
 resource "cloudflare_record" "aaaa_host_fqdn" {
@@ -13,7 +13,7 @@ resource "cloudflare_record" "aaaa_host_fqdn" {
   value   = var.cf_host_ipv6
   type    = "AAAA"
   ttl     = var.cf_record_ttl
-  proxied = var.cf_record_proxied
+  proxied = false
 }
 
 resource "cloudflare_record" "a_host_phishlets" {
@@ -30,24 +30,6 @@ resource "cloudflare_record" "aaaa_host_phishlets" {
   name    = var.cf_host_phishlets
   value   = var.cf_host_ipv6
   type    = "AAAA"
-  ttl     = var.cf_record_ttl
-  proxied = var.cf_record_proxied
-}
-
-resource "cloudflare_record" "a_domain" {
-  zone_id = var.cf_zone_id
-  name    = var.cf_domain
-  value   = var.cf_host_ipv4
-  type    = "A"
-  ttl     = var.cf_record_ttl
-  proxied = var.cf_record_proxied
-}
-
-resource "cloudflare_record" "a_www_domain" {
-  zone_id = var.cf_zone_id
-  name    = "www.${var.cf_domain}"
-  value   = var.cf_host_ipv4
-  type    = "A"
   ttl     = var.cf_record_ttl
   proxied = var.cf_record_proxied
 }
