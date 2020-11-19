7f3777e63b7a8b8bbd4dece7329ac189634fc1ec
Justin-p <justin-p@users.noreply.github.com>
2020-11-19 10:45:42 +0100

    removed comments

--- ansible_and_terraform/hetzer_cloudflare/terraform_playbooks/infrastructure/variables.tf
+++ ansible_and_terraform/hetzer_cloudflare/terraform_playbooks/infrastructure/variables.tf
@@ -2,14 +2,12 @@ variable "name" {
   default     = "gophish"
 }
 
-# Resource random to generate random characters
 resource "random_string" "name" { 
   length = 6
   special = false
   upper = false
 }
 
-# The designation of the name, which is built from the variable name + environment terraform + result random
 locals {                        
   name = "${var.name}-${terraform.workspace}-${random_string.name.result}"
 }
